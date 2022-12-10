33708cc7496fa3beb75cbd51b273fda60062281f
sayus2884 <joniah2884@gmail.com>
2022-12-10 14:24:49 -0800

    Add cors

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 import 'dotenv/config'
+import routes from "./routes";
+import cors from "cors";
 
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-  res.send('Hello World!')
-})
+app.use(cors());
+
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
