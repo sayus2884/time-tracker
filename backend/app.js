b3d2e8a338aff0e7804e1fcf6691a89d7baa15cc
sayus2884 <joniah2884@gmail.com>
2022-12-10 20:57:46 -0800

    Add body parser

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,17 @@ import cors from "cors";
 import routes from "./routes";
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 8080;
 
 app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use("/users", routes.users);
 
